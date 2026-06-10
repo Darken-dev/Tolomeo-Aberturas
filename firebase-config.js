05f4a4bcc4845c9ab77b6b8dc50d3cca26cdf220
Darken-dev <manuelfernandezf010@gmail.com>
2026-06-10 17:51:01 -0300

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,17 @@
+// ╔══════════════════════════════════════════════════════════╗
+// ║  firebase-config.js                                     ║
+// ║  ¡IMPORTANTE! Este archivo está en .gitignore           ║
+// ║  NO lo subas a GitHub nunca.                            ║
+// ║  Mantenelo solo en tu computadora y en el servidor.     ║
+// ╚══════════════════════════════════════════════════════════╝
+
+const FIREBASE_CONFIG = {
+  apiKey: "AIzaSyB_3dI3r7xtNw__MXX20mnCAksZnpQCD9U",
+  authDomain: "tolomeopuertasyventnas.firebaseapp.com",
+  databaseURL: "https://tolomeopuertasyventnas-default-rtdb.firebaseio.com",
+  projectId: "tolomeopuertasyventnas",
+  storageBucket: "tolomeopuertasyventnas.firebasestorage.app",
+  messagingSenderId: "12516780742",
+  appId: "1:12516780742:web:d02cf94fbde8c9498545c7",
+  measurementId: "G-V3DTZWJCN6"
+};
